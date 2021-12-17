e04c7c9483fa07a3933a94e7c2ce14a5ad352c73
vitaliiyz <vz2601@mail.ru>
2021-12-17 19:13:05 +0300

    3-5 lessons (6 beginning)

---
+++ src/com/teachmeskills/lesson6/task1/Transport.java
@@ -0,0 +1,8 @@
+package com.teachmeskills.lesson6.task1;
+
+public abstract class Transport {
+    protected int power;
+    protected int speed;
+    protected double weight;
+    protected String brand;
+}
