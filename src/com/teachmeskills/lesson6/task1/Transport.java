a9d5d1d0fbbe38fa9c1ecd9015f7f8155f469817
vitaliiyz <vz2601@mail.ru>
2021-12-18 19:45:25 +0300

    6: added classes

--- src/com/teachmeskills/lesson6/task1/Transport.java
+++ src/com/teachmeskills/lesson6/task1/Transport.java
@@ -5,4 +5,12 @@ public abstract class Transport {
     protected int speed;
     protected double weight;
     protected String brand;
+
+
+    public Transport(int power, int speed, double weight, String brand) {
+        this.power = power;
+        this.speed = speed;
+        this.weight = weight;
+        this.brand = brand;
+    }
 }
